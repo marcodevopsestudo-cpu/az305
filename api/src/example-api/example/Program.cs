0beae84ed143ca00ef781a51cd8422a85f8342c6
marcodevopsestudo-cpu <marco.devops.estudo@gmail.com>
2025-11-03 01:06:41 -0300

    updt
    asa

--- api/src/example-api/example/Program.cs
+++ api/src/example-api/example/Program.cs
@@ -1,25 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(o =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    o.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+    o.RoutePrefix = "swagger"; // ui at /swagger
+});
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.MapGet("/health", () => Results.Ok("OK"));
 
 app.Run();
