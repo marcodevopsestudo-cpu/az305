1cede0f1ae3482d1b9330803465599987e0a3da2
marcodevopsestudo-cpu <marco.devops.estudo@gmail.com>
2025-11-04 12:05:35 -0300

    upd

---
+++ infra/modules/oidc/main.tf
@@ -0,0 +1,23 @@
+resource "azuread_application" "app" {
+  display_name = "${var.name_prefix}-gha-oidc"
+}
+
+resource "azuread_service_principal" "sp" {
+  client_id = azuread_application.app.client_id
+}
+
+resource "azuread_application_federated_identity_credential" "fic" {
+  for_each              = toset(var.oidc_subjects)
+  application_id = azuread_application.app.id
+  
+  # Prefixa com "gha-" e troca qualquer caractere inválido por "-"
+  display_name = regexreplace("gha-${each.value}", "[^0-9A-Za-z._-]", "-")
+
+  issuer    = "https://token.actions.githubusercontent.com"
+  audiences = ["api://AzureADTokenExchange"]
+
+  # o subject continua exatamente como o GitHub emite
+  subject   = each.value
+
+  description = "OIDC GitHub Actions"
+}
---
+++ infra/modules/oidc/variables.tf
@@ -0,0 +1,7 @@
+variable "name_prefix"  { type = string }
+variable "github_owner" { type = string }
+variable "github_repo"  { type = string }
+variable "github_branch" { type = string }
+variable "oidc_subjects" {
+  type = list(string)
+}
